acf21c289c37fcb9d39bfce68c6a54ae091862ac
KovVitalii <149310749+KovVitalii@users.noreply.github.com>
2023-11-05 19:50:32 +0100

    Update Main01.java

--- src/Main01.java
+++ src/Main01.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main01 {
-
+// типа что то новое добавил =/
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     while (true) {
