42ce016e8608425e9dd40786a3fbdd9cd6ea22b9
Vitalii Kovtun <kovvv1992@gmail.com>
2023-11-05 19:16:15 +0100

    make addition calculator

--- src/Hallo.java
+++ src/Hallo.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class Hallo {
 
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+    System.out.println(a + b);
   }
-
 }
